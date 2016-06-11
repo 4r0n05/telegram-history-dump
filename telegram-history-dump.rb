342453644f3640b59dff50cd4d3df136d655492b
4r0n05 <4r0n05@users.noreply.github.com>
2016-06-11 23:03:38 +0200

    Implement timeout retry

--- telegram-history-dump.rb
+++ telegram-history-dump.rb
@@ -12,6 +12,9 @@ require_relative 'lib/cli_parser'
 require_relative 'lib/dump_progress'
 require_relative 'lib/util'
 
+class RetryError < Exception
+end
+
 Dir[File.dirname(__FILE__) + '/formatters/*.rb'].each do |file|
   require File.expand_path(file)
 end
@@ -59,9 +62,21 @@ def dump_dialog(dialog)
                 offset + $config['chunk_size']
               ])
     msg_chunk = nil
-    Timeout::timeout($config['chunk_timeout']) do
-      msg_chunk = exec_tg_command('history', dialog['print_name'],
-                                  $config['chunk_size'], offset)
+    retry_count = 0
+    while retry_count <= $config["chunk_retry"] do
+        begin 
+            Timeout::timeout($config['chunk_timeout']) do
+              msg_chunk = exec_tg_command('history', dialog['print_name'],
+                                          $config['chunk_size'], offset)
+            end
+            break
+        rescue Timeout::Error
+            if retry_count == $config["chunk_retry"]
+                raise RetryError
+            end
+            $log.error('Timeout, retrying... (%d/%d)' % [retry_count + 1, $config["chunk_retry"]])
+            retry_count += 1;
+        end
     end
     raise 'Expected array' unless msg_chunk.is_a?(Array)
     msg_chunk.reverse_each do |msg|
@@ -281,14 +296,14 @@ $log.info('Backing up %d dialogs: %s' % [
 
 $dumper.start_backup
 backup_list.each_with_index do |dialog,i|
-  sleep($config['chunk_delay']) if i > 0
   begin
+    sleep($config['chunk_delay']) if i > 0
     connect_socket
     dump_dialog(dialog)
-    save_progress
-  rescue Timeout::Error
-    $log.error('Command timeout, skipping to next dialog')
+  rescue RetryError
+    $log.error('Timeout, skipping to next dialog')
     disconnect_socket
+    save_progress
   end
 end
 $dumper.end_backup
